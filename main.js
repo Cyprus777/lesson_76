267b414bd3733a59f3a869442c11e6bcc86ede45
Cyprus777 <133809276+Cyprus777@users.noreply.github.com>
2023-12-17 12:30:14 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+// let - переменные
+let firstName = 'John'
+firstName = 'Alex'
+
+let age = 20
+
+// var - устаревший способ объявления переменных
+var lastName = 'Smith'
+
+// const - константа
+const firstName1 = 'Ivan'
+const firstName2 = 'Ivanov'
+// при изменении константы браузер выдаст ошибку
+// firstName1 = 'Anton' 
+
+console.log(firstName)
+console.log(age)
+console.log(firstName1)
+console.log(lastName)
+console.log(firstName1, firstName2)
+console.log(age, 'лет')
+console.log(age, 'age')
+console.log(age, 'y.o')
+console.log(firstName1, firstName2, age, 'y.o')
+
+age = 30
+console.log(age)
